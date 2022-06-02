1d0ae1f3c6019ed84b9c4d2877e62e3bb7e96dca
RÔMULO RODRIGUES DE OLIVEIRA <rrorodriguesoliveira@gmail.com>
2022-06-02 01:30:30 -0300

    secret-remove

--- src/api/index.js
+++ src/api/index.js
@@ -3,26 +3,6 @@ import axios from "axios";
 const URL =
   "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
-/* const options = {
-  params: {
-    bl_latitude: "11.847676",
-    tr_latitude: "12.838442",
-    bl_longitude: "109.095887",
-    tr_longitude: "109.149359",
-    restaurant_tagcategory_standalone: "10591",
-    restaurant_tagcategory: "10591",
-    limit: "30",
-    currency: "USD",
-    open_now: "false",
-    lunit: "km",
-    lang: "en_US",
-  },
-  headers: {
-    "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
-    "X-RapidAPI-Key": "ca00c32c3emsh3856d52348de47ap15e3ebjsn4115096291aa",
-  },
-}; */
-
 export const getPlacesData = async (bounds) => {
   try {
     const { data: data } = await axios.get(URL, {
